e03fea74c97db5b2e3eba0d04687dfc20b2f234b
Isaac Lloyd <isaac@isaaclloyd.work>
2022-06-16 16:48:44 -0700

    faster

--- index.js
+++ index.js
@@ -36,109 +36,83 @@ async function main() {
 	const len = 26
 
 	for (var a = 0; a < len; a++) {
-		//console.log('a: ' + a)
 		count.a++
 		for (var b = 0; b < len; b++) {
-			//if (check([a, b])) continue
-			//console.log('b: ' + b)
+			if (check([a, b])) continue
 			count.b++
 			for (var c = 0; c < len; c++) {
-				//if (check([a, b, c])) continue
-				//console.log('c: ' + c)
+				if (check([a, b, c])) continue
 				count.c++
 				for (var d = 0; d < len; d++) {
-					//if (check([a, b, c, d])) continue
-					//console.log('d: ' + d)
+					if (check([a, b, c, d])) continue
 					for (var e = 0; e < len; e++) {
-						//if (check([a, b, c, d, e])) continue
-						//console.log('e: ' + e)
+						if (check([a, b, c, d, e])) continue
 						count.e++
 						for (var f = 0; f < len; f++) {
-							//if (check([a, b, c, d, e, f])) continue
-							//console.log('f: ' + f)
+							if (check([a, b, c, d, e, f])) continue
 							count.f++
 							for (var g = 0; g < len; g++) {
-								//if (check([a, b, c, d, e, f, g])) continue
-								//console.log('g: ' + g)
+								if (check([a, b, c, d, e, f, g])) continue
 								count.g++
 								for (var h = 0; h < len; h++) {
-									//if (check([a, b, c, d, e, f, g, h])) continue
-									//console.log('h: ' + h)
+									if (check([a, b, c, d, e, f, g, h])) continue
 									count.h++
 									for (var i = 0; i < len; i++) {
-										//if (check([a, b, c, d, e, f, g, h, i])) continue
-										//console.log('i: ' + i)
+										if (check([a, b, c, d, e, f, g, h, i])) continue
 										count.i++
 										for (var j = 0; j < len; j++) {
-											//if (check([a, b, c, d, e, f, g, h, i, j])) continue
-											//console.log('j: ' + j)
+											if (check([a, b, c, d, e, f, g, h, i, j])) continue
 											count.j++
 											for (var k = 0; k < len; k++) {
-												//if (check([a, b, c, d, e, f, g, h, i, j, k])) continue
-												//console.log('k: ' + k)
+												if (check([a, b, c, d, e, f, g, h, i, j, k])) continue
 												count.k++
 												for (var l = 0; l < len; l++) {
-													//if (check([a, b, c, d, e, f, g, h, i, j, k, l])) continue
-													//console.log('l: ' + l)
+													if (check([a, b, c, d, e, f, g, h, i, j, k, l])) continue
 													count.l++
 													for (var m = 0; m < len; m++) {
-														//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m])) continue
-														//console.log('m: ' + m)
+														if (check([a, b, c, d, e, f, g, h, i, j, k, l, m])) continue
 														count.m++
 														for (var n = 0; n < len; n++) {
-															//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n])) continue
-															//console.log('n: ' + n)
+															if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n])) continue
 															count.n++
 															for (var o = 0; o < len; o++) {
-																//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o])) continue
-																//console.log('o: ' + o)
+																if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o])) continue
 																count.o++
 																for (var p = 0; p < len; p++) {
-																	//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p])) continue
-																	//console.log('p: ' + p)
+																	if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p])) continue
 																	count.p++
 																	for (var q = 0; q < len; q++) {
-																		//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q])) continue
-																		//console.log('q: ' + q)
+																		if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q])) continue
 																		count.q++
 																		for (var r = 0; r < len; r++) {
-																			//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r])) continue
-																			//console.log('r: ' + r)
+																			if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r])) continue
 																			count.r++
 																			for (var s = 0; s < len; s++) {
-																				//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s])) continue
-																				//console.log('s: ' + s)
+																				if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s])) continue
 																				count.s++
 																				for (var t = 0; t < len; t++) {
-																					//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t])) continue
-																					//console.log('t: ' + t)
+																					if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t])) continue
 																					count.t++
 																					for (var u = 0; u < len; u++) {
-																						//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u])) continue
-																						//console.log('u: ' + u)
+																						if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u])) continue
 																						count.u++
 																						for (var v = 0; v < len; v++) {
-																							//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v])) continue
-																							//console.log('v: ' + v)
+																							if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v])) continue
 																							count.v++
 																							for (var w = 0; w < len; w++) {
-																								//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w])) continue
-																								//console.log('w: ' + w)
+																								if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w])) continue
 																								count.w++
 																								for (var x = 0; x < len; x++) {
-																									//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x])) continue
-																									//console.log('x: ' + x)
+																									if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x])) continue
 																									count.x++
-																									//stats()
+																									stats()
 																									for (var y = 0; y < len; y++) {
-																										//if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y])) continue
-																										//console.log('y: ' + y)
+																										if (check([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y])) continue
 																										count.y++
 																										for (var z = 0; z < len; z++) {
 																											let abc = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
 																											count.z++
 																											if (check(abc)) continue
-																											//console.log('z: ' + z)
 																											//let abc = [alpha[a], alpha[b], alpha[c]]
 																											//alphagram = alphagram.concat([abc])
 																											//console.log(abc)
